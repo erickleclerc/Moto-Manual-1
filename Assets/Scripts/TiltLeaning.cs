06022d5b61f87180a5ea5b3dec369159149e1277
erickleclerc <107086802+erickleclerc@users.noreply.github.com>
2023-06-15 21:59:09 -0700

    Tilt Leaning Progress

--- Assets/Scripts/TiltLeaning.cs
+++ Assets/Scripts/TiltLeaning.cs
@@ -3,12 +3,9 @@ using UnityEngine;
 public class TiltLeaning : MonoBehaviour
 {
     public float maxTiltAngle = 45f;        // Maximum angle the motorcycle can lean
-    public float maxTurnAngle = 30f;        // Maximum angle the motorcycle can turn
     public float rotationSpeed = .5f;       // Speed of rotation when tilting
-    public float turnIntensity = .5f;        // Intensity of the turn
 
     private float currentTiltAngle = 0f;    // Current angle of motorcycle tilt
-    private float currentTurnAngle = 0f;    // Current angle of motorcycle turn
 
     private Rigidbody rb;
 
@@ -24,46 +21,63 @@ public class TiltLeaning : MonoBehaviour
 
     private void Update()
     {
-        // Get the head tilt angle
-        float headTiltAngle = GetHeadTiltAngle();
-
-        // Calculate the desired tilt angle for the motorcycle based on the head tilt angle
-        float desiredTiltAngle = Mathf.Clamp(headTiltAngle, -maxTiltAngle, maxTiltAngle);
 
 
-        if (rb.velocity.magnitude > 5)
+        float controllerAngle = GetHeadTiltAngle();
+
+        if (controllerAngle > 180)
         {
-            // Smoothly rotate the motorcycle towards the desired tilt angle
-            currentTiltAngle = Mathf.Lerp(currentTiltAngle, desiredTiltAngle, rotationSpeed * Time.deltaTime);
-
-            // Apply the rotation to the motorcycle
-            //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0f, currentTurnAngle, currentTiltAngle), rotationSpeed * Time.deltaTime);
-            //transform.rotation = Quaternion.Euler(0f, currentTurnAngle, currentTiltAngle);
-
-          // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z), rotationSpeed * Time.deltaTime);
-            //NEEDS TO BE FIXED*********
-
-            transform.rotation = Quaternion.Euler(0, 0, currentTiltAngle);
-
-            // Calculate the desired turn angle based on the head tilt angle and turn intensity
-            float desiredTurnAngle = Mathf.Clamp(headTiltAngle * turnIntensity, -maxTurnAngle, maxTurnAngle);
-
+            controllerAngle = controllerAngle - 360;
         }
-       
+
+        float desiredTiltAngle = Mathf.Clamp(controllerAngle, -maxTiltAngle, maxTiltAngle);
+
+
+        Quaternion roll = Quaternion.AngleAxis(desiredTiltAngle, Vector3.forward);
+        Quaternion yaw = Quaternion.AngleAxis(-desiredTiltAngle, Vector3.up);
+
+        // Tilt and lean, yaw ALWAYS come before roll in Quaternions
+        Quaternion rigRotation = yaw * roll;
+
+
+        if (rb.velocity.magnitude > 2)
+        {
+            //Smooth out the rotation
+            transform.rotation = Quaternion.Slerp(transform.rotation, rigRotation, rotationSpeed * Time.deltaTime);
+            
+            
+        }
+        else
+        {
+            // Keep the bike upright when moving under 5km/h
+            Quaternion upright = Quaternion.AngleAxis(0, Vector3.forward);
+            transform.rotation = upright;
+
+
+          
+        }
+
+
+
+        // transform.rotation = rigRotation;
+
+
     }
 
     private float GetHeadTiltAngle()
     {
         headZRotation = head.transform.eulerAngles.z;
-        if (headZRotation > 0f && headZRotation < 100)
-        {
-                tiltedAngle = headZRotation;
-        }
-        else if (headZRotation > 100f )
-        {
-             tiltedAngle = headZRotation - 360f;
+        //if (headZRotation > 0f && headZRotation < 100)
+        //{
+        //        tiltedAngle = headZRotation;
+        //}
+        //else if (headZRotation > 100f )
+        //{
+        //     tiltedAngle = headZRotation - 360f;
            
-        }
-        return tiltedAngle;
+        //}
+        ////return tiltedAngle;
+
+        return headZRotation;
     }
 }
